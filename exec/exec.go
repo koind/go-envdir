fbc8ab274c5f49eafb70a7d8167e9dd37232f5fc
Damir15 <krosava_15d@mail.ru>
2019-07-24 08:54:24 +0600

    New logic

--- exec/exec.go
+++ exec/exec.go
@@ -1,14 +1,28 @@
 package exec
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func Command(commandName, envDir string) {
-	cmd := exec.Command(commandName, envDir)
-	err := cmd.Start()
+	envList, err := getEnv(envDir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = setEnv(envList)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cmd := exec.Command(commandName)
+	err = cmd.Start()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -22,4 +36,38 @@ func Command(commandName, envDir string) {
 	out, err := cmd.CombinedOutput()
 	os.Stdout.Write(out)
 	os.Stderr.Write([]byte(err.Error()))
+}
+
+func getEnv(filePath string) (map[string]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	envList := make(map[string]string, 10)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		texts := strings.Split(scanner.Text(), "=")
+		if len(texts) >= 2 {
+			envList[texts[0]] = texts[1]
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return envList, nil
+}
+
+func setEnv(evnList map[string]string) error {
+	for key, val := range evnList {
+		err := os.Setenv(key, val)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
